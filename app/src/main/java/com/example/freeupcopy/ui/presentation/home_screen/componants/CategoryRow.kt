cd3043dc30b9130bc28c68b2420d3935bd9379b0
sahil <sksahilislam0123@gmail.com>
2024-12-04 10:27:17 +0530

    no changes

--- app/src/main/java/com/example/freeupcopy/ui/presentation/home_screen/componants/CategoryRow.kt
+++ app/src/main/java/com/example/freeupcopy/ui/presentation/home_screen/componants/CategoryRow.kt
@@ -12,21 +12,24 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.freeupcopy.R
+import com.example.freeupcopy.domain.model.Category
 
 @Composable
 fun CategoryRow(
     imageId: Array<Int>,
     names: Array<String>,
     modifier: Modifier = Modifier
-){
-    LazyRow (contentPadding = PaddingValues(16.dp)){
+) {
+    LazyRow(contentPadding = PaddingValues(16.dp)) {
         val itemCount = imageId.size
 
-        items(itemCount){
+        items(itemCount) {
             RowItem(
                 modifier,
-                painter =  imageId,
+                painter = imageId,
                 title = names,
                 itemIndex = it
             )
@@ -40,17 +43,40 @@ fun RowItem(
     painter: Array<Int>,
     title: Array<String>,
     itemIndex: Int
-){
-Card (
-    modifier
-        .padding(10.dp)
-        .fillMaxWidth()
-        .height(100.dp),
-    colors = CardDefaults.cardColors(
-        containerColor = Color.White
-    ),
-    elevation = CardDefaults.cardElevation(10.dp)
-){
+) {
+    Card(
+        modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .height(100.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
 
+    }
 }
+
+@Preview
+@Composable
+fun CategoryRowPreview() {
+    CategoryRow(
+        imageId = arrayOf(
+            R.drawable.p1,
+            R.drawable.p1,
+            R.drawable.p1,
+            R.drawable.p1,
+            R.drawable.p1,
+            R.drawable.p1
+        ),
+        names = arrayOf(
+            "Peperoni",
+            "Vegan",
+            "FourCheese",
+            "Margaritta",
+            "American",
+            "Mexican"
+        )
+    )
 }
